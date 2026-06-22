e3e499e4bde97e97d5f3fcf7555279f2134aa9a9
varis_vecpuisis <varis_vecpuisis@ayloconsultants.com>
2026-06-22 15:56:10 +0300

    implementing playable race list

--- player_races.js
+++ player_races.js
@@ -1,7 +1,7 @@
 var player_races = {};
 
 async function loadPlayerRacesCSV() {
-    const csvUrl= `https://docs.google.com/spreadsheets/d/1FvMqrnt5MnwbhKFfjVkT7HFT3fC8yKnyvrQnPtjxrPQ/export?format=csv?&gid=1936715235`;
+    const csvUrl= `https://docs.google.com/spreadsheets/d/1FvMqrnt5MnwbhKFfjVkT7HFT3fC8yKnyvrQnPtjxrPQ/export?format=csv&gid=1936715235`;
     // const csvUrl = 'https://corsproxy.io/?' + encodeURIComponent(
     //         `https://docs.google.com/spreadsheets/d/1FvMqrnt5MnwbhKFfjVkT7HFT3fC8yKnyvrQnPtjxrPQ/export?format=csv&gid=1936715235&_v=${Date.now()}`
     //     );
