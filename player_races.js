e762719ae5845ec987161c4f859446f6ae0a1ee9
varis_vecpuisis <varis_vecpuisis@ayloconsultants.com>
2026-06-23 12:07:19 +0300

    initial work

--- player_races.js
+++ player_races.js
@@ -69,6 +69,8 @@ async function populatePlayerRaces() {
 
 function genRaceTraitList(t) {
     var all_traits = t.split("|");
+    if (all_traits[0]=='[') {all_traits=all_traits.substring(1,all_traits.length-1)}
+    if (all_traits[all_traits.length-1]==']') {all_traits=all_traits.substring(0,all_traits.length-1)}
     var rez_text = "<ul>";
     for (i of all_traits){
         rez_text += `<li>${i}</li>`;
