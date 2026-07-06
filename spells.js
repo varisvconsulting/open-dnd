d4fa10d3569ce2bbe24ae1315bec223acdb971b8
varis_vecpuisis <varis_vecpuisis@ayloconsultants.com>
2026-07-06 20:46:58 +0300

    fixing tabs vs lists

--- spells.js
+++ spells.js
@@ -151,7 +151,7 @@ function constructCreatureCard(name){
 }
 
 function fillSpellButtonList(){
-    const list = document.getElementById('spell-entry-list');
+    const list = document.getElementById('spell_tab_list_card');
     list.replaceChildren();
 
     for (const i of spell_data) {
@@ -164,12 +164,12 @@ function fillSpellButtonList(){
             <span>${spell_lvl}, ${magic_class}, ${spell_type}</span>
         `;
         list.appendChild(entry);
-        // entry.addEventListener('click', (e) => {
-        //     // retain active for css
-        //    fillSpellListMainCard(e.dataset.name);
-        //    console.log("clicked ", e.dataset.name);
+        entry.addEventListener('click', (e) => {
+            // retain active for css
+           fillSpellListMainCard(e.dataset.name);
+           console.log("clicked ", e.dataset.name);
             
-        // });
+        });
     }
 }
 
