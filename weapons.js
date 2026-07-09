5b7e48ae181f3d6eefd20794c946507d8416fea2
varis_vecpuisis <varis_vecpuisis@ayloconsultants.com>
2026-07-09 22:19:55 +0300

    item work

--- weapons.js
+++ weapons.js
@@ -180,6 +180,9 @@ function updateWeaponGroupFilter(){
 function bindItemButtons(){
     document.querySelectorAll("[data-weapon-category-filter]").forEach((select) => select.addEventListener('change', () => { 
         WEAPON_CATEGORY_FILTER = select.value.toLowerCase().trim();
+        console.log("got pressed category filter: ", WEAPON_CATEGORY_FILTER);
+        console.log("raw select value: ", select.value);
+        console.log("raw select value lowercased: ", select.value.toLowerCase().trim())
         updateWeaponGroupFilter();
     }));
 
