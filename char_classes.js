69bb5193b57bea1dcc37fae1e2cf76b39de48521
varis_vecpuisis <varis_vecpuisis@ayloconsultants.com>
2026-07-14 21:47:43 +0300

    Īmplement class menu

--- char_classes.js
+++ char_classes.js
@@ -8,7 +8,7 @@ async function loadCharClassCSV(){
     const CharClassCsvUrl = 'https://corsproxy.io/?' + encodeURIComponent(
         `https://docs.google.com/spreadsheets/d/1FvMqrnt5MnwbhKFfjVkT7HFT3fC8yKnyvrQnPtjxrPQ/export?format=csv&gid=308079445&_v=${Date.now()}`
     );
-    const list = document.getElementById('armor-list');
+    const list = document.getElementById('chas_class_list');
     list.textContent = `Loading…`;
 
     try {
@@ -23,9 +23,30 @@ async function loadCharClassCSV(){
                     CHAR_CLASS_DATA.class_data[class_name] = []
                     let class_btn_list = document.getElementById("class_tab_buttons");
                     let c_btn = document.createElement("button");
-                    c_btn.classList = ["tab_button"];
+                    c_btn.classList = ["tab_button class_tab_button"];
                     c_btn.innerHTML=class_name;
                     class_btn_list.appendChild(c_btn);
+
+                    let char_menu_panel = document.getElementById("char_class_display");
+                    let c_panel = document.createElement("div");
+                    c_panel.id = `char_class_${class_name}`;
+                    c_btn.addEventListener("click", () => {
+                        // Remove active from all buttons
+                        document.querySelectorAll(".class_tab_button").forEach(btn => {
+                            btn.classList.remove("active");
+                        });
+
+                        // Activate this button
+                        c_btn.classList.add("active");
+
+                        // Hide all panels
+                        document.querySelectorAll(".char_class_panel").forEach(panel => {
+                            panel.style.display = "none";
+                        });
+
+                        // Show this panel
+                        c_panel.style.display = "block";
+                    });
                 }
     
                 if (is_archetype) {
